cb1355895a20097fcd65a9b86203b9af25d9c175
Magnus Anderssen <magnus@magooweb.com>
2020-12-06 08:19:35 +0100

    day 6

---
+++ 6/index.ts
@@ -0,0 +1,34 @@
+import { loadFileBlockSeparatedByAnEmptyLine } from "../loadFile";
+import { print, run, title } from "../utilities";
+
+run(__dirname, (filename) => {
+  const groups = loadFileBlockSeparatedByAnEmptyLine(filename);
+
+  title("One", 1);
+  print(
+    groups
+      .map((line) => {
+        return new Set(line.replace(/ /g, "").split(""));
+      })
+      .reduce((acc, group: Set<string>) => acc + group.size, 0)
+  );
+  title("Two", 1);
+  print(
+    groups
+      .map((line) => {
+        const persons = line.split(" ");
+        const set = new Set(persons[0].split(""));
+
+        for (let i = 1; i < persons.length; i++) {
+          const letters = persons[i].split("");
+          set.forEach((letter) => {
+            if (!letters.includes(letter)) {
+              set.delete(letter);
+            }
+          });
+        }
+        return set.size;
+      })
+      .reduce((acc, count) => acc + count)
+  );
+});
